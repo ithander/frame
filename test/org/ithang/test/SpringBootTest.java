40de0c3623fdbb5a1054bb7ccc9255a522d3f9b8
zyt <3218138121@qq.com>
2019-10-25 11:30:12 +0800

    aaa
    aaaaa

---
+++ test/org/ithang/test/SpringBootTest.java
@@ -0,0 +1,14 @@
+package org.ithang.test;
+
+import org.ithang.BootstrapApplication;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.transaction.annotation.Transactional;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@Transactional
+public class SpringBootTest {
+
+}
